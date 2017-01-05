0ad0f83625d4e1b1d6370ed880f4453be3704795
Unknown <quangnhanbui@gmail.com>
2017-01-05 15:16:30 +0100

    change var names
    changement de nom de variables

--- js.js
+++ js.js
@@ -3,34 +3,34 @@ var currentIndex = 0,
   items = $('.slider div'),
   itemAmt = items.length;
 
-function cycleItems() {
+function carousel() {
   var item = $('.slider div').eq(currentIndex);
   items.hide();
   item.css('display','inline-block');
 }
 
-var autoSlide = setInterval(function() {
+var automatic = setInterval(function() {
   currentIndex += 1;
   if (currentIndex > itemAmt - 1) {
     currentIndex = 0;
   }
-  cycleItems();
-}, 3000);
+  carousel();
+}, 3500);
 
 $('.next').click(function() {
-  clearInterval(autoSlide);
+  clearInterval(automatic);
   currentIndex += 1;
   if (currentIndex > itemAmt - 1) {
     currentIndex = 0;
   }
-  cycleItems();
+  carousel();
 });
 
 $('.prev').click(function() {
-  clearInterval(autoSlide);
+  clearInterval(automatic);
   currentIndex -= 1;
   if (currentIndex < 0) {
     currentIndex = itemAmt - 1;
   }
-  cycleItems();
+  carousel();
 });
