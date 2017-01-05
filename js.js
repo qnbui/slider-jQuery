46e7d17851eb65f989fd5b769dcfda70372f3718
Unknown <quangnhanbui@gmail.com>
2017-01-05 15:36:42 +0100

    Commentaries
    Commentaire sur la page jquery

--- js.js
+++ js.js
@@ -3,12 +3,16 @@ var currentIndex = 0,
   items = $('.slider div'),
   itemLen = items.length;
 
+
+// eq crée un nouvel objet jquery d'un élément et la fonction carousel
+// permet de superposer une image sur un autre
 function carousel() {
   var item = $('.slider div').eq(currentIndex);
   items.hide();
   item.css('display','inline-block');
 }
 
+//la fonction crée un slider automatique
 var automatic = setInterval(function() {
   currentIndex += 1;
   if (currentIndex > itemLen - 1) {
@@ -17,6 +21,10 @@ var automatic = setInterval(function() {
   carousel();
 }, 3500);
 
+//la fonction next marche par un clique sur un bouton dès qu'elle est
+//activé la fonction next permet de changer d'image et elle arrête
+//le slide automatique et elle fonctionne de gauche à droite
+//pour aller à l'image suivante
 $('.next').click(function() {
   clearInterval(automatic);
   currentIndex += 1;
@@ -26,6 +34,8 @@ $('.next').click(function() {
   carousel();
 });
 
+//la fonction prev marche comme la next sauf qu'elle fonctionne
+//de droite à gauche pour revenir à l'image précédente
 $('.prev').click(function() {
   clearInterval(automatic);
   currentIndex -= 1;
